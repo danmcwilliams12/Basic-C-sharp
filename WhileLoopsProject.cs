1dc3401b0f9e8adc73a9c046c9f58ff09a525e9d
Dan <daniel.mcwilliams12@yahoo.com>
2019-05-01 16:51:43 -0400

    Create first half of WhileLoopsProject.cs

---
+++ WhileLoopsProject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace WhileLoopsProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("You have 5 dollars. You need 10 dollars to go see a movie.");
+
+            int money = 5;
+            int moneyNeeded;
+            bool hasMoney = money >= 10;
+            while (!hasMoney)
+            {
+                moneyNeeded = 10 - money;
+                Console.WriteLine("You need "+moneyNeeded+" more dollars to see the movie.");
+                Console.WriteLine("Type \"d\" to gain a dollar");
+                string input = Console.ReadLine().ToLower();
+                if(input == "d")
+                {
+                    money += 1;
+                    hasMoney = money >= 10;
+                }
+                else
+                {
+                    Console.WriteLine("Oops, you didn't type \"d\".  Try again");
+                }
+            }
+            Console.WriteLine("You have enough money now! Enjoy the show.");
+            Console.Read();
+        }
+    }
+}
