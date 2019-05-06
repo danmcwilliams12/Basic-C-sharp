71ba76dc0b05826b01b0f43140aef6a518ea45cc
danmcwilliams12 <daniel.mcwilliams12@yahoo.com>
2019-05-06 11:11:52 -0400

    Create MethodsExercise.cs

---
+++ TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        public Card()
+        {
+            Suit = "Spades";
+            Face = "Two:";
+
+        }
+        public string Face { get; set; }
+        public string Suit { get; set; }
+    }
+}
