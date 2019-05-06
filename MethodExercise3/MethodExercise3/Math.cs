99da5a03aa7db1e611a917afca3ea49ac7e71a6f
danmcwilliams12 <daniel.mcwilliams12@yahoo.com>
2019-05-06 14:36:23 -0400

    Create MethodExercise3.csproj

---
+++ MethodExercise3/MethodExercise3/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise3
+{
+    class Math
+    {
+        public static int AddTogether(int num1, int num2 = 1)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ MethodExercise3/MethodExercise3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number...");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a second number if you wish.  If not leave blank.");
+            string input2 = Console.ReadLine();           
+            if (input2 == "")
+            {
+                Console.WriteLine(Math.AddTogether(input1));
+            }
+            else
+            {
+                Console.WriteLine(Math.AddTogether(input1, Convert.ToInt32(input2)));
+            }                       
+
+            Console.ReadLine();
+        }   
+    }
+}
