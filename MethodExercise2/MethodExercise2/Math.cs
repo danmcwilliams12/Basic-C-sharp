2cfa4e14ba15c149722f98b3b5d553d6324e42e2
danmcwilliams12 <daniel.mcwilliams12@yahoo.com>
2019-05-06 13:45:12 -0400

    Method exercise/Test sln migration
    Include AcmeAccounting.sln

---
+++ MethodExercise2/MethodExercise2/Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise2
+{
+    class Math
+    {
+        public static int DoStuff(int num)
+        {
+            return num -= 10;          
+        }
+        public static int DoStuff(decimal num)
+        {     
+            return (Convert.ToInt32(num) / 3);
+        }
+        public static int DoStuff(string num)
+        {
+            return (Convert.ToInt32(num) * 7);
+        }
+    }
+}
---
+++ MethodExercise2/MethodExercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine(Math.DoStuff(12));
+            
+            Math math2 = new Math();
+            Console.WriteLine(Math.DoStuff(4.7m));
+
+            Math math3 = new Math();
+            Console.WriteLine(Math.DoStuff("4"));
+            Console.ReadLine();
+
+        }
+    }
+}
