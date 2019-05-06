43cbf1713153b0806a07cdfc94c8c346a5ff8a59
danmcwilliams12 <daniel.mcwilliams12@yahoo.com>
2019-05-06 12:21:05 -0400

    Create OddMethodsExercise.csproj

---
+++ OddMethodExersice/OddMethodExersice/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddMethodExersice
+{
+    class Math
+    {
+        public static void StrangeMath(int num1, int num2)
+        {
+            num1 += 7;
+            Console.WriteLine(num2.ToString());
+        }
+    }
+}
---
+++ OddMethodExersice/OddMethodExersice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddMethodExersice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Math.StrangeMath(7, 4);
+
+            Math.StrangeMath(num1: 4, num2: 6);
+            Console.ReadLine();
+        }
+    }
+}
