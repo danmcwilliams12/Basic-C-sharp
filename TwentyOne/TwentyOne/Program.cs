f7d78d732aee3f24b090bc80528bc0efc885ca6d
danmcwilliams12 <daniel.mcwilliams12@yahoo.com>
2019-05-06 12:02:09 -0400

    Add more on methods

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -11,13 +11,17 @@ namespace TwentyOne
         static void Main(string[] args)
         {         
             Deck deck = new Deck();
-            deck = Deck.Shuffle(deck);
+            //deck = Deck.Shuffle(deck);           
+            //deck = Deck.Shuffle(deck: deck, times: 5);            
+            // out params must be declared ex.(out <data type> timesShuffled) on function call
+            deck = Deck.Shuffle(deck, out int timesShuffled, 3);
 
-            foreach(Card card in deck.Cards)
+            foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count());
+            Console.WriteLine("Times shuffled: {0}", timesShuffled);
             Console.ReadLine();
 
             
