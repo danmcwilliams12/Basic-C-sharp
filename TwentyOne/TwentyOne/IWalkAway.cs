260900c757219d175daa09e8ad1e3cc89ea9ed43
danmcwilliams12 <daniel.mcwilliams12@yahoo.com>
2019-05-07 10:25:07 -0400

    Create InterfaceExercise.csproj

---
+++ TwentyOne/TwentyOne/IWalkAway.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    interface IWalkAway
+    {
+        //everything is public in an interface
+        //similar to an abstract, all methods must be instantiated in inheriting class
+        void WalkAway(Player player);       
+    }
+}
--- TwentyOne/TwentyOne/TwentyOneGame.cs
+++ TwentyOne/TwentyOne/TwentyOneGame.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    public class TwentyOneGame: Game
+    public class TwentyOneGame: Game, IWalkAway
     {
         //overrides the Play() method in the inherited class Game
         public override void Play()
@@ -18,6 +18,11 @@ namespace TwentyOne
         {
             Console.WriteLine("21 Players:");
             base.ListPlayers();
+        }
+
+        public void WalkAway(Player player)
+        {
+            throw new NotImplementedException();
         }
     }
 }
